1bf358579a91e5388909224bddf20281dbcd01f8
AjayDec <ajaydec78@gmail.com>
2016-03-05 10:38:39 -0800

    Basic static serve site

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello from express');
+    res.send('Hello from Router route');
 });
-
-
 
 app.listen(port,
     function (err) {
