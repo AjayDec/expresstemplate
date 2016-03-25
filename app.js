7ba8437db05b2208f9bb4980020c73567ba51721
AjayDec <ajaydec78@gmail.com>
2016-03-25 11:28:05 -0700

    EJS: Prepare intial setup, npm install --save ejs

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.set('views', 'src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('Hello from express');
+    res.render('index', {
+        title: 'ejs sample',
+        message: 'Hello from view engine ejs'
+    });
 });
 
 
