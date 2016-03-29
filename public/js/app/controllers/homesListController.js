7515b5ce33ba85e05d53672522f658e712d35c86
AjayDec <ajaydec78@gmail.com>
2016-03-29 13:43:38 -0700

    EJS: Implementing simple homes list contorller

--- public/js/app/controllers/homesListController.js
+++ public/js/app/controllers/homesListController.js
@@ -1,7 +1,35 @@
 (function () {
     var app = angular.module('app');
 
-    app.controller('homesListController', ['$scope', function ($scope) {
+    app.controller('homesListController', ['$scope', '_', 'persistenceService', 'Offline', function ($scope, _, persistenceService, Offline) {
+        $scope.showList = false;
+        var getData = function () {
+            persistenceService.getAll().then(
+                function (homes) {
+                    $scope.homes = homes;
+                    $scope.showList = true;
+                    $scope.showEmptyMessage = (homes.length === 0);
+                },
+                function (error) {
+                    $scope.error = error;
+                });
+        };
+        var lazyGetData = _.debounce(getData, 50);
+
+        Offline.on('confirmed-up', lazyGetData);
+        Offline.on('confirmed-down', lazyGetData);
+        lazyGetData();
+
+        $scope.delete = function (index) {
+            var id = $scope.homes[index].id;
+            persistenceService.delete(id).then(
+                function (result) {
+                    $scope.homes.splice(index, 1);
+                },
+                function (error) {
+                    $scope.error = error;
+                });
+        };
 
     }]);
 }());
